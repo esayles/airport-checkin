4d88535e87f28d81965eae11b980ae6bb257d90b
Evan Sayles <evan.sayles@gmail.com>
2021-02-02 12:44:41 -0500

    prevent IndexOutOfBoundsException

--- src/CheckInSimulator.java
+++ src/CheckInSimulator.java
@@ -15,7 +15,7 @@ public class CheckInSimulator {
         passengers = new ArrayList<>(numPax);
         ArrayList<Integer> times = new ArrayList<>(numPax);
         for (int i = 0; i < numPax; i++) {
-            times.set(i, (int) (Math.random() * duration));
+            times.add(i, (int) (Math.random() * duration));
         }
         Collections.sort(times);
         for (int p = 0; p < numPax; p++) {
